728ccf6cbc897d21ed72de579ef9fbaed2a13a9b
Jonas Schlagenhauf <schlagenhauf_github@mailbox.org>
2020-10-26 22:40:08 +0100

    Use setup.py name field correctly

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="schlagenhauf",
+    name="gitlab2mr",
     version="0.1.0",
     author="Jonas Schlagenhauf",
     description="A gitlab-to-myrepo config helper",
