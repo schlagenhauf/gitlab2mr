bcc9d74069652271f9a1ff107ffda68fc98ca8db
Jonas Schlagenhauf <schlagenhauf_github@mailbox.org>
2021-12-13 19:08:05 +0100

    Apply pyscaffold

--- setup.py
+++ setup.py
@@ -1,29 +1,21 @@
-import setuptools
+"""
+    Setup file for gitlab2mr.
+    Use setup.cfg to configure your project.
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+    This file was generated with PyScaffold 4.1.1.
+    PyScaffold helps you to put up the scaffold of your new Python project.
+    Learn more under: https://pyscaffold.org/
+"""
+from setuptools import setup
 
-setuptools.setup(
-    name="gitlab2mr",
-    version="0.1.0",
-    author="Jonas Schlagenhauf",
-    description="A gitlab-to-myrepo config helper",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/schlagenhauf/gitlab2mr",
-    packages=setuptools.find_packages(),
-    entry_points={
-        'console_scripts': ['gitlab2mr=gitlab2mr.cli:main']
-    },
-    license='GNU General Public License',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        'python-gitlab',
-        'click'
-    ],
-    python_requires='>=3.6'
-)
+if __name__ == "__main__":
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
